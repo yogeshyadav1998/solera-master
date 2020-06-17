2a2553e56cd8c0127d8867c17ce733473a2d160e
Kartikay Raheja <36202142+kartikay1999@users.noreply.github.com>
2020-06-17 14:59:07 +0530

    detailed_api_updated

--- backend/detailed_api/main_v1.py
+++ backend/detailed_api/main_v1.py
@@ -3,11 +3,8 @@ from pymongo import MongoClient
 import re 
 import pymongo
 from bson import ObjectId
-from flask_cors import CORS
-
 #from csvtomongo import csvtomongo
 app=Flask("__main__")
-CORS(app)
 client = MongoClient('localhost',27017)
 #csvtomongo()
 @app.route("/")
@@ -28,7 +25,8 @@ def suggestions():
     
     resp=data_merged.find( { '$or': [ { 'medName':{ '$regex':'^'+inp,'$options':'i'} }, 
                          { 'search_salts': {'$regex':'.'+inp,'$options':'i'} } 
-                       ] }, { "_id": 0, "medName":1,"manufacturer":1,"prescription_req":1,"selling_price":1,"salts":1,"Units in Pack":1,"Pack Size":1,"Unit of Measurement":1, "pack form":1,"in_stock":1,"Introduction":1,"uses":1,"benefits":1,"directions":1,"search_salts":1} ).limit(10)
+                       ] }, { "_id": 0, "medName":1,"manufacturer":1,"prescription_req":1,"selling_price":1,"salts":1,"Units in Pack":1,"Pack Size":1,"Unit of Measurement":1, "pack form":1,"in_stock":1,"Introduction":1,"uses":1,"benefits":1,"directions":1,"side_effects":1,
+	"precautions":1,"pageURL":1,"strength_in_mg":1,"overall_strength":1,"netmeds_price":1,"pharmeasy_price":1,"medlife_price":1,"search_salts":1} ).limit(10)
     
     resp=list(resp)
     print(len(resp))
@@ -42,18 +40,18 @@ def getMedicines():
     print(inp) ## Put here connection URI
     db = client['MedicineDetail']
     data_merged = db["data_merged"]
-    print(data_merged)
-    print(input)
-    #resp = data_merged.find({"medName":input}, { "_id": 0, "medName": 1, "pageURL": 1 , "manufacturer": 1, "pharmeasy_price":1,"onemg_price":1,"netmeds_price":1,'salt':1,'quantity_in_pack':1})
+        #resp = data_merged.find({"medName":input}, { "_id": 0, "medName": 1, "pageURL": 1 , "manufacturer": 1, "pharmeasy_price":1,"onemg_price":1,"netmeds_price":1,'salt':1,'quantity_in_pack':1})
     #resp = list(resp)
     #data_merged.create_index( { 'search_salts' : 1 },pymongo.TEXT )
-    resp=data_merged.find({'medName':inp}, { "_id": 0, "medName":1,"manufacturer":1,"prescription_req":1,"selling_price":1,"salts":1,"Units in Pack":1,"Pack Size":1,"Unit of Measurement":1, "pack form":1,"in_stock":1,"Introduction":1,"uses":1,"benefits":1,"directions":1,"search_salts":1})
+    resp=data_merged.find({'medName':inp}, { "_id": 0, "medName":1,"manufacturer":1,"prescription_req":1,"selling_price":1,"salts":1,"Units in Pack":1,"Pack Size":1,"Unit of Measurement":1, "pack form":1,"in_stock":1,"Introduction":1,"uses":1,"benefits":1,"directions":1,"side_effects":1,
+	"precautions":1,"pageURL":1,"strength_in_mg":1,"overall_strength":1,"netmeds_price":1,"pharmeasy_price":1,"medlife_price":1,"search_salts":1})
     resp=list(resp)
     
     saltname=resp[0]['search_salts']
     print(saltname)
-    result=data_merged.find({'search_salts':saltname}, { "_id": 0, "medName":1,"manufacturer":1,"prescription_req":1,"selling_price":1,"salts":1,"Units in Pack":1,"Pack Size":1,"Unit of Measurement":1, "pack form":1,"in_stock":1,"Introduction":1,"uses":1,"benefits":1,"directions":1,"search_salts":1})
-    print(data_merged.find({'search_salts':saltname}, { "_id": 0, "medName":1,"manufacturer":1,"prescription_req":1,"selling_price":1,"salts":1,"Units in Pack":1,"Pack Size":1,"Unit of Measurement":1, "pack form":1,"in_stock":1,"Introduction":1,"uses":1,"benefits":1,"directions":1,"search_salts":1}).explain())
+    result=data_merged.find({'search_salts':saltname}, { "_id": 0, "medName":1,"manufacturer":1,"prescription_req":1,"selling_price":1,"salts":1,"Units in Pack":1,"Pack Size":1,"Unit of Measurement":1, "pack form":1,"in_stock":1,"Introduction":1,"uses":1,"benefits":1,"directions":1,"side_effects":1,
+	"precautions":1,"pageURL":1,"strength_in_mg":1,"overall_strength":1,"netmeds_price":1,"pharmeasy_price":1,"medlife_price":1,"search_salts":1})
+
     result=list(result)
     return jsonify(output=result)
     
@@ -65,7 +63,8 @@ def filter_api():
     pack_form=request.json['pack_form']
     db = client['MedicineDetail']
     data_merged = db["data_merged"]
-    resp=data_merged.find({'medName':inp}, { "_id": 0, "medName":1,"manufacturer":1,"prescription_req":1,"selling_price":1,"salts":1,"Units in Pack":1,"Pack Size":1,"Unit of Measurement":1, "pack form":1,"in_stock":1,"Introduction":1,"uses":1,"benefits":1,"directions":1,"search_salts":1})
+    resp=data_merged.find({'medName':inp}, { "_id": 0, "medName":1,"manufacturer":1,"prescription_req":1,"selling_price":1,"salts":1,"Units in Pack":1,"Pack Size":1,"Unit of Measurement":1, "pack form":1,"in_stock":1,"Introduction":1,"uses":1,"benefits":1,"directions":1,"side_effects":1,
+	"precautions":1,"pageURL":1,"strength_in_mg":1,"overall_strength":1,"netmeds_price":1,"pharmeasy_price":1,"medlife_price":1,"search_salts":1})
     resp=list(resp)
     
     saltname=resp[0]['search_salts']
@@ -80,7 +79,8 @@ def filter_api():
         filter_arr.append({'pack form':{"$in":pack_form}})
     
     
-    result=data_merged.find( { '$and': filter_arr }, { "_id": 0, "medName":1,"manufacturer":1,"prescription_req":1,"selling_price":1,"salts":1,"Units in Pack":1,"Pack Size":1,"Unit of Measurement":1, "pack form":1,"in_stock":1,"Introduction":1,"uses":1,"benefits":1,"directions":1,"search_salts":1} )
+    result=data_merged.find( { '$and': filter_arr }, { "_id": 0, "medName":1,"manufacturer":1,"prescription_req":1,"selling_price":1,"salts":1,"Units in Pack":1,"Pack Size":1,"Unit of Measurement":1, "pack form":1,"in_stock":1,"Introduction":1,"uses":1,"benefits":1,"directions":1,"side_effects":1,
+	"precautions":1,"pageURL":1,"strength_in_mg":1,"overall_strength":1,"netmeds_price":1,"pharmeasy_price":1,"medlife_price":1,"search_salts":1} )
 
 
     result=list(result)
