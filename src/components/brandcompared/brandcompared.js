15eb905f47b7f94e9f0871eec51e696cf094952c
yogeshyadav1998 <yogeshbhakli@gmail.com>
2020-07-07 09:47:38 +0530

    brand comparision update

--- src/components/brandcompared/brandcompared.js
+++ src/components/brandcompared/brandcompared.js
@@ -7,8 +7,14 @@ class brandcompared extends Component{
         return(
             <div className="brands_section">
                 <p className="brands_heading">We compare all these at once</p>
-                <div>
-                    <img src="/images/img.png"/>
+                <div className="brands_images">
+                    <img style={{width:"15%", margin:"0px 10px"}} src="/images/img.png"/>
+                    <img style={{width:"30%", margin:"0px 10px"}} src="/images/netmeds.png"/>
+                    <img style={{width:"30%", margin:"0px 10px"}} src="/images/medlife.png"/>
+                </div>
+                <div className="brands_images">
+                    <img style={{width:"30%", margin:"0px 10px"}} src="/images/medifarm.png"/>
+                    <img style={{width:"30%", margin:"0px 10px"}} src="/images/pharmeasy.png"/>
                 </div>
             </div>
         )
