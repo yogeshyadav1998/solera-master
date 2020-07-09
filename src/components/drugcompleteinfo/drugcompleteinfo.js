92d44d55724868b74177f0688750c6ca36b60156
yogeshyadav1998 <yogeshbhakli@gmail.com>
2020-07-09 09:16:16 +0530

    deals page added

--- src/components/drugcompleteinfo/drugcompleteinfo.js
+++ src/components/drugcompleteinfo/drugcompleteinfo.js
@@ -17,8 +17,89 @@ class drugcompleteinfo extends Component{
         })
     }
 
+    covertnumber(value){
+        if(Number.isNaN(value)){
+            return 10000
+        }
+        return value
+    }
+
+    minimumprice(medicine){
+        var minimumprice = Math.min(
+            this.covertnumber(Number(medicine.selling_price)),
+            this.covertnumber(Number(medicine.medlife_price)),
+            this.covertnumber(Number(medicine.netmeds_price)),
+            this.covertnumber(Number(medicine.pharmeasy_price)) )
+        return minimumprice
+        
+    }
+    
+    minimumpricewebsite(medicine){
+        var minimumprice = this.minimumprice(medicine)
+        if(Number(medicine.selling_price) == minimumprice){
+            return "ONE-MG"
+        }else if(Number(medicine.medlife_price) == minimumprice){
+            return "MED-LIFE"
+        }else if(Number(medicine.netmeds_price) == minimumprice){
+            return "NET-MED"
+        }else{
+            return "PHARMEASY"
+        }
+    }
+
+
 
     render(){
+
+        let prices = [
+            {
+                name: "ONE-MG",
+                value: this.props.drug.selling_price
+            },
+            {
+                name: "MED-LIFE",
+                value: this.props.drug.medlife_price
+            },
+            {
+                name: "NET-MED",
+                value: this.props.drug.netmeds_price
+            },
+            {
+                name: "PHARMEASY",
+                value: this.props.drug.pharmeasy_price
+            }
+        ]
+
+        let remainingdeals=(
+            prices.map(price =>(
+              <div style={this.minimumpricewebsite(this.props.drug) == price.name ? {display: "none"} : null} className="info_part">
+                  <p className="info_heading">{price.name}</p>
+                  <p style={{fontSize:"1.5rem", margin: "auto"}}>Rs. {price.value}</p>
+              </div>
+            ))
+        )
+
+        let deals =(
+            <div className="info_deals">
+                <div className="info_best_deal">
+                    <div className="info_details">
+                        <p className="info_productname">{this.props.drug.medName}</p>
+                        <p className="info_manufacturername">mfr: {this.props.drug.manufacturer}</p>
+                        <p className="info_best_deal_site">Best price on {this.minimumpricewebsite(this.props.drug)}</p>
+                        <p className="info_best_deal_price">Rs {this.minimumprice(this.props.drug)}</p>
+                        {this.props.drug.prescription_req == "Not Available" ? null : <p style={{color: "white", width:"20%", padding:"5px",margin:"10px" , background:"rgb(66, 172, 166)", border:"1px solide grey", borderRadius:"10px"}}>Rx Required</p> }
+                    </div>
+                   <div className="info_image">
+                       <img src="/images/drug.jfif" />
+                   </div>
+                </div>
+                <div>
+                    <p style={{padding: "10px", fontSize:"1rem", fontWeight: "600"}}>More Deals</p>
+                    {remainingdeals}
+                </div>
+            </div>
+        )
+
         let overview = (
             <div className="info_overview">
                 <div className="info_part">
@@ -57,7 +138,7 @@ class drugcompleteinfo extends Component{
         )
 
         let side_effects = (
-            <div>
+            <div className="info_side_effects">
                 <div className="info_part">
                     <p className="info_heading">Directions</p>
                     <p className="info_content">{this.props.drug.directions}</p>
@@ -73,8 +154,8 @@ class drugcompleteinfo extends Component{
             </div>
         )
 
-        let precaustions = (
-            <div>
+        let precautions = (
+            <div className="info_precautions">
                 <div className="info_part">
                     <p className="info_heading">Precautions</p>
                     <p className="info_content">{this.props.drug.precautions}</p>
@@ -94,12 +175,13 @@ class drugcompleteinfo extends Component{
             <div>
                 <Header/>
                 <div className="info_toggler">
+                    <button className="infotogglebutton" value="deals" onClick={this.setinfotype}>Deals</button>
                     <button className="infotogglebutton" value="overview" onClick={this.setinfotype}>Overview</button>
                     <button className="infotogglebutton" value="side_effects" onClick={this.setinfotype}>Side Effects</button>
-                    <button className="infotogglebutton" onClick={this.setinfotype}>Precautions</button>
+                    <button className="infotogglebutton" value="precautions" onClick={this.setinfotype}>Precautions</button>
                 </div>
                 <div className="info_section">
-                    {this.state.info_type == "overview" ? overview : this.state.info_type == "side_effects" ? side_effects : precaustions}
+                    {this.state.info_type == "overview" ? overview : this.state.info_type == "side_effects" ? side_effects : this.state.info_type == "precautions" ? precautions : deals}
                 </div>
             </div>
         )
--- src/components/header/Heading2.jsx
+++ src/components/header/Heading2.jsx
@@ -12,7 +12,8 @@ function Heading () {
 
     return (
         <div className="heading">
-            <img src="https://www.grxstatic.com/mobile/dd596aeffdc98608df59858c73363537451f4d11/dist/2ec83f525d6b5740a74163993bbdf051.svg"/>
+            <Link style={{textDecoration : "none"}} to="/"><p className="web_title">MeraRx</p></Link>
+            {/* <img src="https://www.grxstatic.com/mobile/dd596aeffdc98608df59858c73363537451f4d11/dist/2ec83f525d6b5740a74163993bbdf051.svg"/> */}
             <div className="language_selector">
                 <button className="language_toggle_button" onClick={()=> handlelanguageChange('en')}>English</button>
                 <button className="language_toggle_button" onClick={()=> handlelanguageChange('hi')}>Hindi</button>
