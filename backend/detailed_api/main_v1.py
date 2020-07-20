34890ecdb6d2f40a4bfa4838e494bf4b21b6495a
Kartikay Raheja <36202142+kartikay1999@users.noreply.github.com>
2020-07-20 18:51:40 +0530

    api updated

--- backend/detailed_api/main_v1.py
+++ backend/detailed_api/main_v1.py
@@ -17,6 +17,33 @@ client = MongoClient('localhost',27017)
 def my_index():
     return render_template("index.html")
 
+
+@app.route("/api/all_drug_data",methods=['GET','POST'])
+def drug_data():
+
+    '''
+    if request.method=='GET':
+        inp=request.args['input']  
+    inp = inp.replace('"', '')
+    print(inp)
+   ''' 
+    db=client['MedicineDetail']
+    data_merged=db["data_merged"]
+    print(data_merged)
+    #resp=data_merged.find({'medName':{'$regex':'^'+inp,'$options':'$i'}}, { "_id": 0, "medName": 1, "pageURL": 1 , "manufacturer": 1, "pharmeasy_price":1,"onemg_price":1,"netmeds_price":1,'search_salts':1,'quantity_in_pack':1}).limit(10)
+    
+    resp=data_merged.find( { '$or': [ { 'medName':{ '$regex':'.*'} }, 
+                         { 'search_salts': {'$regex':'.*'} } 
+                       ] }, { "_id": 0, "medName":1,"manufacturer":1,"prescription_req":1,"selling_price":1,"salts":1,"Units in Pack":1,"Pack Size":1,"Unit of Measurement":1, "pack form":1,"in_stock":1,"Introduction":1,"uses":1,"benefits":1,"directions":1,"side_effects":1,
+	"precautions":1,"pageURL":1,"strength_in_mg":1,"overall_strength":1,"netmeds_price":1,"pharmeasy_price":1,"medlife_price":1,"search_salts":1} )
+    
+    resp=list(resp)
+    print(len(resp))
+    
+    return jsonify(result=resp)
+
+
+
 @app.route("/api/data_merged/get_medicinesSuggestions",methods=['GET','POST'])
 def suggestions():
     if request.method=='GET':
